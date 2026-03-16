825650ac99ce30d105c904613f1ba5f59ea3692c
rnewson1 <rnewson1@my.westga.edu>
2026-03-15 21:13:25 -0400

    Implement class ReturnEvent

--- COMP2320Lab3/src/main/java/edu/westga/comp2320/events/ReturnEvent.java
+++ COMP2320Lab3/src/main/java/edu/westga/comp2320/events/ReturnEvent.java
@@ -1,14 +1,15 @@
 package edu.westga.comp2320.events;
 
-import edu.westga.comp2320.resources.Borrowable;
+import edu.westga.comp2320.resources.Laptop;
 
 /**
  * The ReturnEvent represents the event when a resource
  * is returned to the resource center.
+ *
  */
 public class ReturnEvent extends Event {
 
-    private Borrowable resource;
+    private Laptop resource;
 
     /**
      * Creates a new ReturnEvent with the specified time and resource
@@ -17,18 +18,14 @@ public class ReturnEvent extends Event {
      * @param time the scheduled time of the return event
      * @param resource the resource being returned
      */
-    public ReturnEvent(int time, Borrowable resource) {
+    public ReturnEvent(int time, Laptop resource) {
         super(time);
         this.resource = resource;
     }
 
     @Override
     public Event processEvent() {
-
         System.out.println("Return of " + this.resource + " at time " + this.getTime());
-
-        this.resource.returnItem();
-
         return null;
     }
 }
