73d96cad8ad5aa085c14238d475f30f369205732
rnewson1 <rnewson1@my.westga.edu>
2026-03-15 21:14:39 -0400

    Generate initial events of the resource center

---
+++ COMP2320Lab3/src/main/java/edu/westga/comp2320/Main.java
@@ -0,0 +1,11 @@
+package edu.westga.comp2320;
+
+/**
+ * The main class runs the Resource Center simulation.
+ */
+public class Main {
+    static void main() {
+        ResourceCenter resourceCenter = new ResourceCenter();
+        resourceCenter.runSimulation();
+    }
+}
